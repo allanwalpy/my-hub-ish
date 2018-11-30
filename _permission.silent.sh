40621578d7eacbf73ec27a0ad46d38ad38d59ad4
allan_walpy <allanwalpy@gmail.com>
2018-11-30 12:21:16 +0300

    add no promt version of _permisson.sh script

---
+++ _permission.silent.sh
@@ -0,0 +1,5 @@
+echo "---------------------------------------"
+echo "nb! execute only in scripts root folder"
+echo "---------------------------------------"
+read -p "press enter"
+sudo chmod -R +x ./
