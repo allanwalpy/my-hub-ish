e125db1e249683032664ebf16166958ed1415e7e
allan_walpy <allanwalpy@gmail.com>
2018-11-30 12:07:51 +0300

    update grant_permission command

--- grant_premission.sh
+++ grant_premission.sh
@@ -1,3 +1,5 @@
-sudo chmod +x ./*
-sudo chmod +x ./*/*
-sudo chmod +x ./*/*/*
+echo "---------------------------------------"
+echo "nb! execute only in scripts root folder"
+echo "---------------------------------------"
+read -p "press enter"
+sudo chmod -R +x ./
