2045e72b0ba047333dc0a92c6981c9809e7c5ef1
allan_walpy <allanwalpy@gmail.com>
2026-03-20 23:03:08 +0300

    [feat/conky] tiding up

--- feature/conky/monitor/app.c
+++ feature/conky/monitor/app.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define BUFFER_CHUNK_SIZE 1024
 #define BUFFER_FILE_SIZE 16384
@@ -10,26 +11,30 @@ int main() {
 		char *keyword = "FILE_FLASH\n";
 		char *filename = "/tmp/monitor";
 
-		FILE *file = NULL;
 		char buffer_file[BUFFER_FILE_SIZE] = {0};
 		char buffer_chunk[BUFFER_CHUNK_SIZE] = {0};
 
+		FILE *file = NULL;
+		file = fopen(filename, "w");
+
 		while(true) {
 				fgets(buffer_chunk, BUFFER_CHUNK_SIZE, stdin);
-		
+
 				if (strcmp(buffer_chunk, keyword) != 0) {
 						strcat(buffer_file, buffer_chunk);
 						continue;
 				}
 
-				file = fopen(filename, "w");			
+				ftruncate(fileno(file), 0);
 				fprintf(file, "%s", buffer_file);
-				fclose(file);
+				fflush(file);
+				rewind(file);
 
 				buffer_file[0] = '\0';
 				buffer_chunk[0] = '\0';
 		}
 
+		fclose(file);
 		return 0;
 }
 
